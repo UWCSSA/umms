db9f1f2dbd643927743b26f8132fa53b6fb00ffa
Zack Wang <ziquan.wang@yahoo.com>
2015-10-18 20:26:36 -0400

    update database schema

--- conf/members.sql
+++ conf/members.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 12, 2014 at 01:09 AM
--- Server version: 5.6.20
--- PHP Version: 5.5.14
+-- Generation Time: Oct 19, 2015 at 12:25 AM
+-- Server version: 5.6.27
+-- PHP Version: 5.5.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,21 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `members` (
-  `memid` int(10) unsigned NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `memid` int(10) NOT NULL DEFAULT '0',
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
   `credit` int(10) unsigned NOT NULL DEFAULT '0',
-  `regdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `regdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `school` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `program` varchar(50) NOT NULL,
+  `sid` varchar(8) NOT NULL,
+  `mobile` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `members`
---
-
-INSERT INTO `members` (`memid`, `fname`, `lname`, `email`, `credit`, `regdate`) VALUES
-(0, 'Ziquan', 'Wang', 'ziquan.wang@yahoo.com', 0, '2014-09-12 01:08:04'),
-(1, 'æ¢“æƒ', 'çŽ‹', 'ziuqan.wang@yahoo.com', 0, '2014-09-12 01:08:58');
 
 --
 -- Indexes for dumped tables
@@ -51,7 +49,7 @@ INSERT INTO `members` (`memid`, `fname`, `lname`, `email`, `credit`, `regdate`) 
 -- Indexes for table `members`
 --
 ALTER TABLE `members`
- ADD PRIMARY KEY (`memid`);
+ ADD PRIMARY KEY (`email`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
