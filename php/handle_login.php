2c5c8a392fb055d70802e8583d48d3014424a1ef
Ran Ji <jran012@hotmail.com>
2015-11-20 15:24:15 -0800

    fix layout

--- php/handle_login.php
+++ php/handle_login.php
@@ -21,28 +21,32 @@
       $stmt->fetch();
 
       // use PHP native GD library to create an e-card image
-      $card_width = 800;
-      $card_height = 500;
+      $card_width = 400;
+      $card_height = 250;
       $name_height_offset = 15; // same as font-size
-      $card_height_offset = 20;
-      $card_owner_text = $user_fname . " " . $user_lname;
+      $card_height_offset = 24;
+      $card_owner_text = strtoupper($user_fname) . "  " . strtoupper($user_lname);
       $register_year = substr($user_regdate, 0, 4);
       $user_memid_padding = $register_year.str_pad(strval($user_memid), 8, "0", STR_PAD_LEFT);
       $card_number_text = substr($user_memid_padding, 0, 4)."  ".
                           substr($user_memid_padding, 4, 4)."  ".
                           substr($user_memid_padding, 8, 4);
+      $width_start_position = 0.3;
+      if (strlen($card_owner_text) < 16) {
+        $width_start_position = $width_start_position + (16 - strlen($card_owner_text))*0.03;
+      }
       $image = imagecreatefrompng("../img/card.png");
       // set text color to be white
-      $textcolorname = imagecolorallocate($image, 241, 236, 144);
+      $textcolorname = imagecolorallocate($image, 239, 220, 155);
       $textcolorcard = imagecolorallocate($image, 255, 255, 255);
-      $fontname = "../img/Bold.ttf";
-      $fontcard = "../img/Bold.ttf";
+      $fontname = "../img/arial.ttf";
+      $fontcard = "../img/kredit_front.ttf";
       // Write the string
       // array imagettftext ( resource $image , float $size , float $angle , int $x , int $y ,
       //                      int $color , string $fontfile , string $text )
-      imagettftext($image, 15, 0, $card_width*0.27, $card_height*0.36+$name_height_offset,
+      imagettftext($image, $name_height_offset, 0, $card_width*$width_start_position, $card_height*0.72+$name_height_offset,
                    $textcolorname, $fontname, $card_owner_text);
-      imagettftext($image, 20, 0, $card_width*0.042, $card_height*0.21+$name_height_offset,
+      imagettftext($image, $card_height_offset, 0, $card_width*0.084, $card_height*0.42+$name_height_offset,
                    $textcolorcard, $fontcard, $card_number_text);
 
       // A walk around to use base64_encode for GD
